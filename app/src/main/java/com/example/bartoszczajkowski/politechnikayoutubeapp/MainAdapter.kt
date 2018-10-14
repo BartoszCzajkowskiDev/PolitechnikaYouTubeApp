7699daad1f159c3b0e0a46d9e3b02739591b0351
bartoszczajkowski <bpczajkowski@gmail.com>
2018-10-14 18:35:09 +0200

    Customized RecyclerView

--- app/src/main/java/com/example/bartoszczajkowski/politechnikayoutubeapp/MainAdapter.kt
+++ app/src/main/java/com/example/bartoszczajkowski/politechnikayoutubeapp/MainAdapter.kt
@@ -4,28 +4,32 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
+
 
 class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
 
+    val videoTitles = listOf("First title", "Second", "3rd", "MOOOOORE TITLE")
+
     override fun getItemCount(): Int {
-        return 3
+        return videoTitles.size
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
-        val layoutInflater = LayoutInflater.from(p0?.context)
-        val cellForRow = layoutInflater.inflate(R.layout.video_row, p0, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
         return CustomViewHolder(cellForRow)
     }
 
 
-    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
-//        val videoTitle = videoTitles.get(position)
-//        holder?.view?.textView_video_title?.text = videoTitle
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val videoTitle = videoTitles.get(position)
+        holder?.view?.textView_video_title?.text = videoTitle
     }
 
 
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
 
 }
