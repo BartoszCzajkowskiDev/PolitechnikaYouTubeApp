2bacb66de6d0c0313e53592532ca1260bf73d287
bartoszczajkowski <bpczajkowski@gmail.com>
2018-10-15 08:26:25 +0200

    Added methods to pass data between activities

--- app/src/main/java/com/example/bartoszczajkowski/politechnikayoutubeapp/CourseDetailActivity.kt
+++ app/src/main/java/com/example/bartoszczajkowski/politechnikayoutubeapp/CourseDetailActivity.kt
@@ -8,9 +8,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
-
-
+import okhttp3.*
+import java.io.IOException
 
 
 class CourseDetailActivity : AppCompatActivity() {
@@ -22,12 +23,17 @@ class CourseDetailActivity : AppCompatActivity() {
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
         recyclerView_main.adapter = CourseDetailAdapter()
+
+        val navBarTitle = intent.getStringExtra(CustomViewHolder.VIDEO_TITLE_KEY)
+        supportActionBar?.title = navBarTitle
+
+        fetchJson()
     }
 
     private class CourseDetailAdapter: RecyclerView.Adapter<CourseLessonViewHolder>() {
 
         override fun getItemCount(): Int {
-            return 2
+            return 5
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, p1: Int): CourseLessonViewHolder {
@@ -43,9 +49,46 @@ class CourseDetailActivity : AppCompatActivity() {
         }
 
     }
-    
+
     private class CourseLessonViewHolder(val customView: View): RecyclerView.ViewHolder(customView) {
 
+    }
+
+
+    fun fetchJson() {
+
+        val videoId = intent.getIntExtra(CustomViewHolder.VIDEO_ID_KEY, -1)
+        val courseDetailURL = "https://api.letsbuildthatapp.com/youtube/course_detail?id=" + videoId
+
+//        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+//        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        val request = Request.Builder().url(courseDetailURL).build()
+
+
+        client.newCall(request).enqueue(object: Callback {
+
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val courseLessons = gson.fromJson(body, Array<CourseLesson>::class.java)
+
+                //val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+//                runOnUiThread {
+//                    recyclerView_main.adapter = MainAdapter(homeFeed)
+//                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+        })
     }
 
 }
